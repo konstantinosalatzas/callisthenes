ca93d301388d1b23c70b3e7ca888bd8d319c981f
Konstantinos Alatzas <kpalatzas@windowslive.com>
2026-01-03 10:19:16 +0000

    Create was_created_recently()

--- training/models.py
+++ training/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 class Training(models.Model):
     training_title = models.CharField(max_length=200)
@@ -6,6 +9,9 @@ class Training(models.Model):
 
     def __str__(self):
         return self.training_title
+
+    def was_created_recently(self):
+        return self.training_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Performance(models.Model):
     training = models.ForeignKey(Training, on_delete=models.CASCADE)
