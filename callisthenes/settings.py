a408db922bfd783ef61866855420898bcdcd9333
Konstantinos Alatzas <kpalatzas@windowslive.com>
2026-01-04 06:52:28 +0000

    Remove polls app

--- callisthenes/settings.py
+++ callisthenes/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'training.apps.TrainingConfig',
-    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
