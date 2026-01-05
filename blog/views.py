d3c2e298031371b2548bcddffb87f8c9e11af033
Konstantinos Alatzas <kpalatzas@windowslive.com>
2026-01-05 09:22:49 +0000

    Remove drafts

--- blog/views.py
+++ blog/views.py
@@ -43,11 +43,6 @@ def post_edit(request, pk):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 @login_required
-def post_draft_list(request):
-    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
-    return render(request, 'blog/post_draft_list.html', {'posts': posts})
-
-@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method=='POST':
