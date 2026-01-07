cd320747dcadb513786974f01259ecef23f3966b
Konstantinos Alatzas <kpalatzas@windowslive.com>
2026-01-07 16:54:26 +0000

    Create form widgets

--- tracker/forms.py
+++ tracker/forms.py
@@ -9,4 +9,6 @@ class TrainingForm(forms.ModelForm):
         fields = ('title', 'text', 'training_date', 'reps', 'sets')
         widgets = {
             'training_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'reps': forms.NumberInput(attrs={'type': 'number', 'min': '0', 'class': 'form-control'}),
+            'sets': forms.NumberInput(attrs={'type': 'number', 'min': '0', 'class': 'form-control'}),
         }
