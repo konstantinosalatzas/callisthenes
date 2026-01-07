9e1822d9e79efe5f003f0a25423172d5a6b5d470
Konstantinos Alatzas <kpalatzas@windowslive.com>
2026-01-07 17:13:01 +0000

    Update forms fields

--- tracker/forms.py
+++ tracker/forms.py
@@ -6,7 +6,7 @@ class TrainingForm(forms.ModelForm):
 
     class Meta:
         model = Training
-        fields = ('title', 'text', 'training_date', 'sets', 'reps')
+        fields = ('title', 'text', 'training_date', 'sets', 'reps', 'resistance_weight')
         widgets = {
             'training_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
             'sets': forms.NumberInput(attrs={'type': 'number', 'min': '1', 'class': 'form-control'}),
