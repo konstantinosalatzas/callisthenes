e3ee7e46b6d7fbf2b47add99c4ceb2f62aca374f
Konstantinos Alatzas <kpalatzas@windowslive.com>
2026-01-07 17:02:05 +0000

    Update min value

--- tracker/forms.py
+++ tracker/forms.py
@@ -9,6 +9,6 @@ class TrainingForm(forms.ModelForm):
         fields = ('title', 'text', 'training_date', 'sets', 'reps')
         widgets = {
             'training_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
-            'sets': forms.NumberInput(attrs={'type': 'number', 'min': '0', 'class': 'form-control'}),
-            'reps': forms.NumberInput(attrs={'type': 'number', 'min': '0', 'class': 'form-control'}),
+            'sets': forms.NumberInput(attrs={'type': 'number', 'min': '1', 'class': 'form-control'}),
+            'reps': forms.NumberInput(attrs={'type': 'number', 'min': '1', 'class': 'form-control'}),
         }
