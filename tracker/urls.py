eda94c3fdc0097ff512aa47e809578d7074917c8
Konstantinos Alatzas <kpalatzas@windowslive.com>
2026-01-08 13:02:21 +0000

    Update urls

--- tracker/urls.py
+++ tracker/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('training/<int:pk>/edit/', views.training_edit, name='training_edit'),
     path('training/<int:pk>/publish/', views.training_publish, name='training_publish'),
     path('training/<int:pk>/remove/', views.training_remove, name='training_remove'),
+    path('set/<int:pk>/', views.set_detail, name='set_detail'),
+    path('set/<int:pk>/edit/', views.set_edit, name='set_edit'),
+    path('set/<int:pk>/publish/', views.set_publish, name='set_publish'),
+    path('set/<int:pk>/remove/', views.set_remove, name='set_remove'),
 ]
