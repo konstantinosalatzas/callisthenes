efb7b372abb64ecf0946a7fa534507fe756307d6
Konstantinos Alatzas <kpalatzas@windowslive.com>
2026-01-10 13:22:10 +0000

    Create meal model

--- tracker/models.py
+++ tracker/models.py
@@ -26,3 +26,17 @@ class Set(models.Model):
 
     def __str__(self):
         return "{}, set no.{}".format(self.training.title, self.set_number)
+
+class Meal(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    meal_date = models.DateField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
