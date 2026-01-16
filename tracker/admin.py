f1bf333e2e79d23f603f8754cf1b09fbc680ef8d
konstantinosalatzas <kpalatzas@windowslive.com>
2026-01-16 16:31:24 +0200

    Add ingredient model in admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Training, Set, Meal
+from .models import Training, Set, Meal, Ingredient
 
 admin.site.register(Training)
 admin.site.register(Set)
 
 admin.site.register(Meal)
+admin.site.register(Ingredient)
