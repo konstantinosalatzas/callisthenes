ac92c4aa3228efeb36945fb453733b2f13d2f0e8
konstantinosalatzas <kpalatzas@windowslive.com>
2026-01-17 15:40:10 +0200

    Add comment

--- tracker/models.py
+++ tracker/models.py
@@ -8,7 +8,7 @@ class Training(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
     training_date = models.DateField(blank=True, null=True)
-    sets = models.IntegerField(default=0)
+    sets = models.IntegerField(default=0) # calculated from training sets
 
     def publish(self):
         self.published_date = timezone.now()
