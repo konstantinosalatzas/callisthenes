5c94d5a2f921f436dead42cbcebd867404e84ce6
konstantinosalatzas <kpalatzas@windowslive.com>
2026-01-19 11:24:31 +0200

    Add unit model in admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import Training, Set, Meal, Ingredient
+from .models import Training, Set, Meal, Ingredient, Unit
 
 admin.site.register(Training)
 admin.site.register(Set)
 
 admin.site.register(Meal)
 admin.site.register(Ingredient)
+
+admin.site.register(Unit)
