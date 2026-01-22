f77c789315e947ff2a3c5bbc3e9d78e36124511d
konstantinosalatzas <kpalatzas@windowslive.com>
2026-01-22 15:21:23 +0200

    Test unit calories calculation with integer macronutrient values

--- tracker/tests.py
+++ tracker/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# TODO: Create tests.
+from .models import Unit
+
+class UnitModelTests(TestCase):
+    def test_calculate_calories_with_integer_macronutrient_values(self):
+        unit = Unit(protein=20, carbs=1, fats=1)
+        self.assertEqual(unit.calculate_calories(), (20 * 4 + 1 * 4 + 1 * 9))
