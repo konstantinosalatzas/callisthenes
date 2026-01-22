c6cd77c3a71d1c5347d4b092c8f7e6a81515fb49
konstantinosalatzas <kpalatzas@windowslive.com>
2026-01-22 15:24:10 +0200

    Test unit calories calculation with float macronutrient values

--- tracker/tests.py
+++ tracker/tests.py
@@ -6,3 +6,7 @@ class UnitModelTests(TestCase):
     def test_calculate_calories_with_integer_macronutrient_values(self):
         unit = Unit(protein=20, carbs=1, fats=1)
         self.assertEqual(unit.calculate_calories(), (20 * 4 + 1 * 4 + 1 * 9))
+
+    def test_calculate_calories_with_float_macronutrient_values(self):
+        unit = Unit(protein=20.5, carbs=0.5, fats=0.5)
+        self.assertEqual(unit.calculate_calories(), (20.5 * 4 + 0.5 * 4 + 0.5 * 9))
