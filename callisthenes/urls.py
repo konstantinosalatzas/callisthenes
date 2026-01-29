f294cee5be002b6dbd062021d2f937235be74cea
konstantinosalatzas <kpalatzas@windowslive.com>
2026-01-29 14:51:03 +0200

    Add password change

--- callisthenes/urls.py
+++ callisthenes/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('accounts/password_change/', views.PasswordChangeView.as_view(), name='password_change'),
+    path('accounts/password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
