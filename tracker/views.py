bd1776f4aaad03776d78c8b015f16ed69a841e06
konstantinosalatzas <kpalatzas@windowslive.com>
2026-01-29 10:06:55 +0200

    Move sign up view to tracker views

--- tracker/views.py
+++ tracker/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 from .models import Training, Set, Meal, Ingredient, Unit
 from .forms import TrainingForm, SetForm, MealForm, IngredientForm, UnitForm
 
 def index(request):
     return render(request, 'tracker/index.html')
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
 
 # Training model
 
