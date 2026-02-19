6050c27ef20f733b485e71d38ce06248cfc89cb8
konstantinosalatzas <kpalatzas@windowslive.com>
2026-02-19 14:44:21 +0200

    Rename total

--- tracker/views.py
+++ tracker/views.py
@@ -179,9 +179,9 @@ def meal_remove(request, pk):
 def ingredient_detail(request, pk):
     ingredient = get_object_or_404(Ingredient, pk=pk)
     get_object_or_404(Meal, pk=ingredient.meal.pk, user=request.user)
-    (p1, p2, p3, p1_p2, total_macros) = macronutrient_percentages(ingredient.pk)
+    (p1, p2, p3, p1_p2, total) = macronutrient_percentages(ingredient.pk)
     return render(request, 'tracker/ingredient_detail.html', {'ingredient': ingredient,
-                                                              'p1': p1, 'p2': p2, 'p3': p3, 'p1_p2': p1_p2, 'total_macros': total_macros})
+                                                              'p1': p1, 'p2': p2, 'p3': p3, 'p1_p2': p1_p2, 'total': total})
 
 @login_required
 def ingredient_new(request, pk):
