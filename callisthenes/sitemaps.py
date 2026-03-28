ea938ecbf428cd33aac2aed0ba2f4b9bce9210da
konstantinosalatzas <kpalatzas@windowslive.com>
2026-03-28 09:21:51 +0200

    Format code

--- callisthenes/sitemaps.py
+++ callisthenes/sitemaps.py
@@ -1,7 +1,6 @@
 from django.contrib import sitemaps
 from django.urls import reverse
 
-
 class StaticViewSitemap(sitemaps.Sitemap):
     def items(self):
         return ["index", "signup", "login", "logout", "password_change", "training_list", "training_new", "meal_list", "meal_new", "unit_list", "unit_new"]
