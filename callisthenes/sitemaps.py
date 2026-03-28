239301d5cbcc23f30513c5a46daf2afd67ef665d
konstantinosalatzas <kpalatzas@windowslive.com>
2026-03-28 09:19:39 +0200

    Update sitemap urls

--- callisthenes/sitemaps.py
+++ callisthenes/sitemaps.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 class StaticViewSitemap(sitemaps.Sitemap):
     def items(self):
-        return ["index", "signup", "training_list", "training_new", "meal_list", "meal_new", "unit_list", "unit_new"]
+        return ["index", "signup", "login", "logout", "password_change", "training_list", "training_new", "meal_list", "meal_new", "unit_list", "unit_new"]
 
     def location(self, item):
         return reverse(item)
